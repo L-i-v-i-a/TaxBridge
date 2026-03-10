b781cec081ad831db27a1ae21d7e4fcfe8f383b9
Mishaelx6 <mishaeljohnson56@gmail.com>
2026-03-10 15:05:40 +0000

    correction

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,14 +7,22 @@ import Price from "./pages/price";
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <div className="bg-[#0D23AD] text-white relative overflow-hidden" style={{backgroundImage: 'url(/image.png)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundBlendMode: 'overlay'}}>
-      <Nav />
-      <section className="max-w-7xl mx-auto px-6 py-24 text">
-
-      </section>
-      <Hero />
+      <div
+        className="bg-[#0D23AD] text-white relative overflow-hidden"
+        style={{
+          backgroundImage: "url(/image.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundBlendMode: "overlay",
+        }}
+      >
+        <Nav />
+        <section className="max-w-7xl mx-auto px-6 py-24 text">
+          <Hero />
+        </section>
       </div>
+
+      <Price />
     </main>
   );
-  return <Price />;
 }
