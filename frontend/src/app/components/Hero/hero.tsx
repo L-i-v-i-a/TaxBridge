16e6df2ba570651dfea2f5055565697a1fb7454a
pheetomilechi <tomilechighajiga@gmail.com>
2026-03-10 14:54:08 +0000

    Made changes to the landing page and navigation components to enhance the design and layout. Updated the background of the landing page to include an image with a blend effect, and adjusted the navigation bar's styling for better visual appeal.

--- frontend/src/app/components/Hero/hero.tsx
+++ frontend/src/app/components/Hero/hero.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Nav from '../nav/nav';
 export default function Hero() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold text-center mt-2 text-[#5FF7E2]">
+        <div className="min-h-screen flex flex-col items-center justify-start px-6 pt-0 pb-10">
+            <h1 className="text-4xl font-bold text-center text-[#5FF7E2] leading-tight mb-2">
                 TAX FILLING THAT ACTUALLY FITS YOUR LIFE
             </h1>
-            <p className="text-lg text-left mt-5 text-white">
-                Real humans on <br />demand + AI accuracy.  Fully compliant refund without the stress.
-            </p>
+            <div className="w-190 max-w-7xl">
+                <p className="text-3xl text-left text-white font-bold">
+                   <span> Real humans on demand + AI accuracy.
+                    <br />
+                    Fully compliant refund without the stress.
+                    </span>
+                </p>
+            </div>
         </div>
-        
     );
 }
