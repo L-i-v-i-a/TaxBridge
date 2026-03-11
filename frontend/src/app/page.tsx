6c557fb3077dac1107317cf0c4810df211c73648
Mishaelx6 <mishaeljohnson56@gmail.com>
2026-03-11 11:39:02 +0000

    corrected pulled error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,18 +16,15 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, [router]);
 
-
-export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-[#0D23AD]">
       <div className="text-center">
         <h1 className="animate-fade-in-out text-4xl font-bold text-white">
-         TAXBRIDGE<br/>
-         US Tax Calculation Software
+          TAXBRIDGE
+          <br />
+          US Tax Calculation Software
         </h1>
       </div>
-
-    
     </main>
   );
 }
