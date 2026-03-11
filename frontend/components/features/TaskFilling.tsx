659efa9c2187102af44421500f190fe146e7800a
Mishaelx6 <mishaeljohnson56@gmail.com>
2026-03-11 12:56:49 +0000

    hero section fixed

--- frontend/components/features/TaskFilling.tsx
+++ frontend/components/features/TaskFilling.tsx
@@ -1,7 +1,7 @@
-import "TaskFilling" from "./features/TaskFilling";
-
-return (
-  <div className="min-h-screen bg-[#0D23AD] flex flex-col items-center">
-    {/* Navbar would go here */}
-  </div>   
-)    
+export default function TaskFilling() {
+  return (
+    <div className="min-h-screen bg-[#0D23AD] flex flex-col items-center">
+      {/* Navbar would go here */}
+    </div>
+  )
+}
