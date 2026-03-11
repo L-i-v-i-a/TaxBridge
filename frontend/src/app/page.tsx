8c2cdce23e6bd22daf85d8f3676f2ccb2c26239b
Mishaelx6 <mishaeljohnson56@gmail.com>
2026-03-11 08:11:37 +0000

    worked on page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 import Hero from "./components/Hero/hero";
 import Nav from "./components/nav/nav";
 
-import Price from "./pages/price";
+
 
 export default function Home() {
   return (
@@ -22,7 +22,7 @@ export default function Home() {
         </section>
       </div>
 
-      <Price />
+    
     </main>
   );
 }
