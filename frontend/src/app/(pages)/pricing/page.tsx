b20844bc399be53e1eaac126563c37394deae3e9
Mishaelx6 <mishaeljohnson56@gmail.com>
2026-03-11 11:25:39 +0000

    pricing

--- frontend/src/app/(pages)/pricing/page.tsx
+++ frontend/src/app/(pages)/pricing/page.tsx
@@ -1,11 +1,15 @@
 import Nav from "../../../../components/nav"
 import Pricing from "../../../../components/Pricing"
+import NewsletterSection from "../../../../components/NewsletterSection";
+import SiteFooter from "../../../../components/SiteFooter";
 
 export default function PricingPage() {
   return (
     <>
     <Nav/>
     <Pricing/>
+    <NewsletterSection/>
+    <SiteFooter/>
     </>
   )
 }
